4a1305c1f31dcd947bbfc7c8ac47589f4cf74a7e
Chen Shuo <546537825@qq.com>
2016-07-06 18:13:28 +0800

    Test Service.

--- src/nm/service/ServiceClient.js
+++ src/nm/service/ServiceClient.js
@@ -5,7 +5,7 @@ export default class ServiceClient {
     getUserPlayLists(uid = "78843035") {
         return new Promise((resolve, reject) => {
             $.ajax({
-                url: `${NM_API_URL}/user/playlist/`;
+                url: `${NM_API_URL}/user/playlist/`,
                 data: {
                     uid,
                     limit: 1000,
@@ -17,24 +17,34 @@ export default class ServiceClient {
                 } else {
                     reject("Response with error code:" + res.code);
                 }
-            });
+            }, reject);
         });
     }
 
     getPlayListDetail(id) {
         return new Promise((resolve, reject) => {
             $.ajax({
-                url: `${NM_API_URL}/user/playlist/detail`;
+                url: `${NM_API_URL}/playlist/detail`,
                 data: {
                     id
                 }
             }).then(res => {
                 if (res.code === 200 ) {
-                    resolve(res.result.tracks);
+                    resolve(res.result);
                 } else {
                     reject("Response with error code:" + res.code);
                 }
-            });
+            }, reject);
         });
     }
+}
+
+
+let __instance = null;
+ServiceClient.getInstance = function()
+{
+    if(__instance === null) {
+        __instance = new ServiceClient();
+    }
+    return __instance;
 }
