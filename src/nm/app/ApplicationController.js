f96057d57339b20c3d4d564b778f430363ec366e
Chen Shuo <546537825@qq.com>
2016-07-14 15:25:16 +0800

    Add interaction.

--- src/nm/app/ApplicationController.js
+++ src/nm/app/ApplicationController.js
@@ -5,10 +5,41 @@ import ServiceClient from "../service/ServiceClient";
 
 export default class ApplicationController extends NJUApplicationController
 {
+    init()
+    {
+        super.init();
+        this._playLists = [];
+        this._selectedPlayList = null;
+    }
+
+    get playLists()
+    {
+        return this._playLists;
+    }
+    set playLists(value)
+    {
+        this._playLists = value;
+        this._onPlayListsChanged();
+    }
+
+    get selectedPlayList()
+    {
+        return this._selectedPlayList;
+    }
+    set selectedPlayList(value)
+    {
+        if (this.selectedPlayList !== value)
+        {
+            this._selectedPlayList = value;
+            this._onSelectedPlayListChanged();
+        }
+    }
+
     createApplication()
     {
         const application = new Application();
         this.playListView = application.playListView;
+        this.playListView.on("selectionchanged", this._playListView_selectionchanged.bind(this));
         this.trackTableView = application.trackTableView;
         return application;
     }
@@ -19,15 +50,46 @@ export default class ApplicationController extends NJUApplicationController
 
         try {
             await ServiceClient.getInstance().login();
-            this.playListView.items = await ServiceClient.getInstance().getUserPlayLists();
-            this.playListView.selection = this.playListView.items[0];
-            const playlist = await ServiceClient.getInstance().getPlayListDetail(this.playListView.items[10].id);
-            this.trackTableView.items = playlist.tracks;
-            console.log(playlist.tracks);
+            await this._loadUserPlayList();
+
         }
         catch (e)
         {
             console.log(e);
         }
+    }
+
+
+    async _loadUserPlayList()
+    {
+        this.playLists = await ServiceClient.getInstance().getUserPlayLists();
+
+        if (this.playLists.length > 0)
+        {
+            this.playListView.selection = this.playLists[0];
+        }
+    }
+
+    _onPlayListsChanged()
+    {
+        this.playListView.items = this.playLists;
+    }
+
+    _onSelectedPlayListChanged()
+    {
+        if (this.selectedPlayList !== null)
+        {
+            this.trackTableView.items = this.selectedPlayList.tracks;
+        }
+        else
+        {
+            this.trackTableView.items = [];
+        }
+    }
+
+    async _playListView_selectionchanged(e)
+    {
+        const playList = await ServiceClient.getInstance().getPlayListDetail(this.playListView.selectedId);
+        this.selectedPlayList = playList;
     }
 }
