93d41767e223c773749c118e1b401252c496cd97
Chen Shuo <546537825@qq.com>
2016-07-15 14:38:09 +0800

    Modify searchview.

--- src/nm/view/SearchView.js
+++ src/nm/view/SearchView.js
@@ -1,4 +1,5 @@
 import View from "../../nju/view/View";
+import ListView from "../../nju/view/ListView";
 
 export default class SearchView extends View
 {
@@ -7,12 +8,16 @@ export default class SearchView extends View
         super.init();
         this.addStyleClass("nm-search-view");
 
+        this._text = null;
         this.$element.append(`<span class="icon iconfont icon-search"/>`);
         this.$input = $(`<input type=search placeholder="搜索音乐">`);
         this.$element.append(this.$input);
+        this._initSuggestView();
 
         this.$element.on("keydown", this._onkeydown.bind(this));
         this.$element.on("click", "span.icon", this._icon_onclick.bind(this));
+        this.$element.on("input", this._suggest_oninput.bind(this));
+
     }
 
     get text()
@@ -23,6 +28,13 @@ export default class SearchView extends View
     set text(value)
     {
         this.$input.val(typeof(value) === "string" ? value.trim() : "");
+    }
+
+    _initSuggestView()
+    {
+        this.suggestView = new ListView("suggest-view");
+        this.suggestView.addStyleClass("nm-suggest-view");
+        this.addSubView(this.suggestView);
     }
 
     search(text = this.text)
@@ -45,5 +57,10 @@ export default class SearchView extends View
     _icon_onclick(e)
     {
         this.search();
+    }
+
+    _suggest_oninput(e)
+    {
+        console.log("_suggest_oninput");
     }
 }
