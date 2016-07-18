2f3b3894069ff0cfd4d6566e1de6407105cba57e
Chen Shuo <546537825@qq.com>
2016-07-18 10:10:40 +0800

    Use abstract method.

--- src/nju/app/ApplicationController.js
+++ src/nju/app/ApplicationController.js
@@ -39,7 +39,7 @@ export default class ApplicationController extends ViewController
 
     createApplication(options = {})
     {
-        return new Application();
+        throw new Error("createApplication(options) must be overrided in the derived class.");
     }
 
     run()
